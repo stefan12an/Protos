05e7984ab4c3cd43b89b0cc3bff6ecb3d80797fe
stefan12an <vms1238@gmail.com>
2021-07-13 23:27:27 +0300

    New sign-up/login design

--- app/src/main/java/com/example/protos/SignupActivity.java
+++ app/src/main/java/com/example/protos/SignupActivity.java
@@ -2,15 +2,14 @@ package com.example.protos;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,21 +17,13 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.FirebaseUserMetadata;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 
 public class SignupActivity extends AppCompatActivity {
     private EditText etEmail, etPassword;
-    private Button regBtn;
+    private CardView regBtn;
     private TextView logBtn;
     private FirebaseAuth mAuth;
     DatabaseReference databaseReference;
@@ -41,13 +32,16 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         etEmail = findViewById(R.id.email_edt_text);
         etPassword = findViewById(R.id.pass_edt_text);
         regBtn = findViewById(R.id.signup_btn);
         logBtn = findViewById(R.id.login);
         mAuth = FirebaseAuth.getInstance();
         databaseReference = FirebaseDatabase.getInstance("https://protos-dde67-default-rtdb.europe-west1.firebasedatabase.app/").getReference("users");
-        getSupportActionBar().setTitle("Register now!");
+//        getSupportActionBar().setTitle("Register now!");
 
 
         regBtn.setOnClickListener(view -> {
